8200d0b429609a526cc546b96cf9e36fa492bdd4
Gioabu <gioo.abuashvili@gmail.com>
2024-10-13 16:35:50 +0400

    third commit

---
+++ level2/homework.py
@@ -0,0 +1,12 @@
+name = "Giorgi"
+age = "22"
+print ("მე მქვია " + name + " და მე ვარ " + age + " წლის")
+
+favorite_food = "მწვადი"
+print("ჩემი საყვარელი საჭმელი არის " + favorite_food)
+
+favorite_color = "სტაფილოსფერი"
+print("ჩემი საყვარელი ფერი არის " + favorite_color)
+
+
+
