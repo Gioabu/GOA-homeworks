66393dd7275950cd041bb4e9e4cccf7ce53993fa
Gioabu <gioo.abuashvili@gmail.com>
2024-10-14 16:52:22 +0400

    commit

---
+++ level4/homework.py
@@ -0,0 +1,37 @@
+#1) შევქმენი ორი რიცხვის (a და b) ცვლადები, გამოვიყენე მათემატიკურ ოპერაციებში და გამოვიტანე.
+
+a = 20
+b = 10
+
+num1 = a + b
+num2 = a - b 
+num3 = a * b
+num4 = a / b
+
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+
+#2) შევქმენი 2 ცვლადი სახელი და ასაკი, გამოვიყენე წინადადებაში და გამოვიტანე.
+
+name = "Giorgi"
+age = "22"
+
+print("My name is " + name + " and I'm " + age + " years old")
+
+#3) შევქმენი ცვლადი points და შევცვალე მისი მნიშვნელობა და საბოლოო გამოვიტანე.
+
+points = 50
+points = 30
+points = 20
+
+print(points)
+
+#4) დავწერე კოდი რომელიც ითვლის რამდენი წელია გასული ჩემი დაბადების დღიდან.
+
+birthday = 2002
+current_year = 2024
+
+print(current_year - birthday)
+
